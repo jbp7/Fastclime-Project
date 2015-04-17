fa00538ded1c55a2f36d2ba9f20b3b66f838a979
jbp7 <john.pura@duke.edu>
2015-04-17 16:17:52 -0400

    Code v2

---
+++ Python_src_v2/_parametric.pyx
@@ -0,0 +1,35 @@
+
+import numpy as np
+cimport numpy as np
+
+np.import_array()
+
+# cdefine the signature of the c function
+cdef extern from "parametric.h":
+    void parametric(double *SigmaInput, int *m1, double *mu_input, 
+                    double *lambdamin, int *nlambda, int *maxnlambda, double *iicov);
+    
+def mainfunc(np.ndarray[double, ndim=2, mode="c"] SigmaInput not None, 
+             double lambdamin, 
+             int nlambda):   
+
+    #Dimensions    
+    cdef int m1 = SigmaInput.shape[1]
+ 
+    #Define output
+    cdef int maxnlambda = 0
+    cdef np.ndarray mu_input = np.zeros((m1,nlambda), dtype = np.float64, order='C')
+    cdef np.ndarray iicov = np.zeros((m1*m1,nlambda), dtype = np.float64, order='C')
+
+    print ("There is an error")
+    #Call external C function
+    parametric(<double*> np.PyArray_DATA(SigmaInput),
+               &m1,
+               <double*> np.PyArray_DATA(mu_input),
+               <double*> &lambdamin,
+               &nlambda,
+               &maxnlambda,
+               <double*> np.PyArray_DATA(iicov)
+              )
+    
+    return (SigmaInput,mu_input,maxnlambda,iicov)
