4b86cf348825749b1b2a6ab76c23b984160bcbed
jbp7 <john.pura@duke.edu>
2015-04-30 15:37:45 -0400

    added CAPME tests

---
+++ Final_Report/CAPME/parametric2.h
@@ -0,0 +1,4 @@
+
+int ratio_test(double *dy, int *idy,int ndy, double *y, double mu);
+void parametric2(double *SigmaInput_xx, double *SigmaInput_xy, int *m1, double *mu_input, double *lambdamin, int *nlambda, int *maxnlambda, double *iicov);
+void solver2(int m,int n,int nz,int *ia, int *ka, double *a,double *b, double *c);
